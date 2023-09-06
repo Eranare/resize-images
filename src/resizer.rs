0039e8315b961518185a426ffc58d75e62cc6548
Rick <eralon18@gmail.com>
2023-09-06 23:19:53 +0200

    swapped to druid

--- src/resizer.rs
+++ src/resizer.rs
@@ -1,13 +1,9 @@
 // src/resizer.rs
 
-extern crate image;
-use image::{GenericImageView, ImageFormat};
+use image::{ImageFormat};
 use std::fs;
-use std::sync::{Arc, Mutex};
-use gtk::ProgressBar;
 
-
-pub fn resize_images(input_folder: &str, output_folder: &str, target_width: u32, target_height: u32, progress: Arc<Mutex<ProgressBar>>, total_images: usize) {
+pub fn resize_images(input_folder: &str, output_folder: &str, target_width: u32, target_height: u32, progress: &mut f64, total_images: usize) {
     let mut processed_images = 0;
     // Iterate through the input folder and resize each image
     for entry in fs::read_dir(input_folder).expect("Failed to read input directory") {
@@ -20,17 +16,14 @@ pub fn resize_images(input_folder: &str, output_folder: &str, target_width: u32,
             let output_path = format!("{}/{}", output_folder, path.file_name().unwrap().to_str().unwrap());
             resized.save_with_format(output_path, ImageFormat::Png).expect("Failed to save image");
             
-            // Update the progress bar (this is a placeholder, you'd need logic to calculate actual progress)
+            // Update the progress value
             processed_images += 1;
-            let fraction = processed_images as f64 / total_images as f64;
-            let mut progress_lock = progress.lock().unwrap();
-            progress_lock.set_fraction(fraction);
-
+            *progress = processed_images as f64 / total_images as f64;
         }
     }
 }
 
-fn count_images_in_directory(path: &str, include_subfolders: bool) -> usize {
+pub fn count_images_in_directory(path: &str, include_subfolders: bool) -> usize {
     let mut count = 0;
     for entry in fs::read_dir(path).expect("Failed to read directory") {
         let entry = entry.expect("Failed to read entry");
@@ -43,6 +36,3 @@ fn count_images_in_directory(path: &str, include_subfolders: bool) -> usize {
     }
     count
 }
-
-pub use self::resize_images;
-pub use self::count_images_in_directory;
